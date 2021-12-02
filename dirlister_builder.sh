13ab716b2456db014fa9b45617c764054f57fb9a
anmabe <80114034+anmabe06@users.noreply.github.com>
2021-12-02 20:11:05 +0100

    Improve installation

--- dirlister_builder.sh
+++ dirlister_builder.sh
@@ -8,6 +8,7 @@
 #                              #
 ################################
 
+
 #Make the terminal load dirlister.sh every single time, to have access to the command
-echo "source $(pwd)/.dirlister.sh" >> "$file_to_upload_to1"
-echo "source $(pwd)/.dirlister.sh" >> "$file_to_upload_to2"
+echo "source $(pwd)/.dirlister.sh" >> "~/.bashrc"
+echo "source $(pwd)/.dirlister.sh" >> "~/.zshrc"
